da7dc263deca1bd2660711277c4617e6fa3510cc
Ludovico Pestarino <ludovicopestarino00@gmail.com>
2023-01-08 22:40:27 +0100

    moved db connection to db_config.php

--- www/html/db_config.php
+++ www/html/db_config.php
@@ -3,4 +3,10 @@ $servername = "database";
 $username = "root";
 $db_password = "root";
 $dbname = "demo";
+
+$conn = new mysqli($servername, $username, $db_password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
 ?>
